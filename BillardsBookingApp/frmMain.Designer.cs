e31cc5df7cd015baa8e81791a880c1675731b72b
chuhaiphu <phudvse171839@fpt.edu.vn>
2023-11-15 21:14:00 +0700

    Form Structure

--- BillardsBookingApp/frmMain.Designer.cs
+++ BillardsBookingApp/frmMain.Designer.cs
@@ -33,19 +33,25 @@
             menuStripItem_accountInfo = new ToolStripMenuItem();
             logoutToolStripMenuItem = new ToolStripMenuItem();
             loginToolStripMenuItem = new ToolStripMenuItem();
-            controlToolStripMenuItem = new ToolStripMenuItem();
+            manageToolStripMenuItem = new ToolStripMenuItem();
             menuStripItem_clubs = new ToolStripMenuItem();
             menuStripItem_members = new ToolStripMenuItem();
+            tablesToolStripMenuItem = new ToolStripMenuItem();
+            slotsToolStripMenuItem = new ToolStripMenuItem();
+            bookingsToolStripMenuItem = new ToolStripMenuItem();
             aboutToolStripMenuItem = new ToolStripMenuItem();
             applicationToolStripMenuItem = new ToolStripMenuItem();
             contactToolStripMenuItem = new ToolStripMenuItem();
+            viewToolStripMenuItem = new ToolStripMenuItem();
+            clubToolStripMenuItem1 = new ToolStripMenuItem();
+            bookingsHistoryToolStripMenuItem = new ToolStripMenuItem();
             menuStrip_Control.SuspendLayout();
             SuspendLayout();
             // 
             // menuStrip_Control
             // 
             menuStrip_Control.ImageScalingSize = new Size(20, 20);
-            menuStrip_Control.Items.AddRange(new ToolStripItem[] { accountToolStripMenuItem, controlToolStripMenuItem, aboutToolStripMenuItem });
+            menuStrip_Control.Items.AddRange(new ToolStripItem[] { accountToolStripMenuItem, manageToolStripMenuItem, viewToolStripMenuItem, aboutToolStripMenuItem });
             menuStrip_Control.Location = new Point(0, 0);
             menuStrip_Control.Name = "menuStrip_Control";
             menuStrip_Control.Size = new Size(1582, 28);
@@ -64,6 +70,7 @@
             menuStripItem_accountInfo.Name = "menuStripItem_accountInfo";
             menuStripItem_accountInfo.Size = new Size(210, 26);
             menuStripItem_accountInfo.Text = "Show Information";
+            menuStripItem_accountInfo.Click += menuStripItem_accountInfo_Click;
             // 
             // logoutToolStripMenuItem
             // 
@@ -79,24 +86,42 @@
             loginToolStripMenuItem.Text = "Login";
             loginToolStripMenuItem.Click += loginToolStripMenuItem_Click;
             // 
-            // controlToolStripMenuItem
+            // manageToolStripMenuItem
             // 
-            controlToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { menuStripItem_clubs, menuStripItem_members });
-            controlToolStripMenuItem.Name = "controlToolStripMenuItem";
-            controlToolStripMenuItem.Size = new Size(55, 24);
-            controlToolStripMenuItem.Text = "View";
+            manageToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { menuStripItem_clubs, menuStripItem_members, tablesToolStripMenuItem, slotsToolStripMenuItem, bookingsToolStripMenuItem });
+            manageToolStripMenuItem.Name = "manageToolStripMenuItem";
+            manageToolStripMenuItem.Size = new Size(77, 24);
+            manageToolStripMenuItem.Text = "Manage";
             // 
             // menuStripItem_clubs
             // 
             menuStripItem_clubs.Name = "menuStripItem_clubs";
-            menuStripItem_clubs.Size = new Size(224, 26);
-            menuStripItem_clubs.Text = "Clubs";
+            menuStripItem_clubs.Size = new Size(154, 26);
+            menuStripItem_clubs.Text = "Club";
             // 
             // menuStripItem_members
             // 
             menuStripItem_members.Name = "menuStripItem_members";
-            menuStripItem_members.Size = new Size(224, 26);
+            menuStripItem_members.Size = new Size(154, 26);
             menuStripItem_members.Text = "Members";
+            // 
+            // tablesToolStripMenuItem
+            // 
+            tablesToolStripMenuItem.Name = "tablesToolStripMenuItem";
+            tablesToolStripMenuItem.Size = new Size(154, 26);
+            tablesToolStripMenuItem.Text = "Tables";
+            // 
+            // slotsToolStripMenuItem
+            // 
+            slotsToolStripMenuItem.Name = "slotsToolStripMenuItem";
+            slotsToolStripMenuItem.Size = new Size(154, 26);
+            slotsToolStripMenuItem.Text = "Slots";
+            // 
+            // bookingsToolStripMenuItem
+            // 
+            bookingsToolStripMenuItem.Name = "bookingsToolStripMenuItem";
+            bookingsToolStripMenuItem.Size = new Size(154, 26);
+            bookingsToolStripMenuItem.Text = "Bookings";
             // 
             // aboutToolStripMenuItem
             // 
@@ -108,14 +133,34 @@
             // applicationToolStripMenuItem
             // 
             applicationToolStripMenuItem.Name = "applicationToolStripMenuItem";
-            applicationToolStripMenuItem.Size = new Size(224, 26);
+            applicationToolStripMenuItem.Size = new Size(169, 26);
             applicationToolStripMenuItem.Text = "Application";
+            applicationToolStripMenuItem.Click += applicationToolStripMenuItem_Click;
             // 
             // contactToolStripMenuItem
             // 
             contactToolStripMenuItem.Name = "contactToolStripMenuItem";
-            contactToolStripMenuItem.Size = new Size(224, 26);
+            contactToolStripMenuItem.Size = new Size(169, 26);
             contactToolStripMenuItem.Text = "Contact";
+            // 
+            // viewToolStripMenuItem
+            // 
+            viewToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { clubToolStripMenuItem1, bookingsHistoryToolStripMenuItem });
+            viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            viewToolStripMenuItem.Size = new Size(55, 24);
+            viewToolStripMenuItem.Text = "View";
+            // 
+            // clubToolStripMenuItem1
+            // 
+            clubToolStripMenuItem1.Name = "clubToolStripMenuItem1";
+            clubToolStripMenuItem1.Size = new Size(224, 26);
+            clubToolStripMenuItem1.Text = "Club";
+            // 
+            // bookingsHistoryToolStripMenuItem
+            // 
+            bookingsHistoryToolStripMenuItem.Name = "bookingsHistoryToolStripMenuItem";
+            bookingsHistoryToolStripMenuItem.Size = new Size(224, 26);
+            bookingsHistoryToolStripMenuItem.Text = "Bookings History";
             // 
             // frmMain
             // 
@@ -138,7 +183,7 @@
         #endregion
 
         private MenuStrip menuStrip_Control;
-        private ToolStripMenuItem controlToolStripMenuItem;
+        private ToolStripMenuItem manageToolStripMenuItem;
         private ToolStripMenuItem menuStripItem_clubs;
         private ToolStripMenuItem aboutToolStripMenuItem;
         private ToolStripMenuItem applicationToolStripMenuItem;
@@ -150,5 +195,11 @@
         private ToolStripMenuItem menuStripItem_members;
         private ToolStripMenuItem contactToolStripMenuItem;
         private ToolStripMenuItem loginToolStripMenuItem;
+        private ToolStripMenuItem tablesToolStripMenuItem;
+        private ToolStripMenuItem slotsToolStripMenuItem;
+        private ToolStripMenuItem bookingsToolStripMenuItem;
+        private ToolStripMenuItem viewToolStripMenuItem;
+        private ToolStripMenuItem clubToolStripMenuItem1;
+        private ToolStripMenuItem bookingsHistoryToolStripMenuItem;
     }
 }
--- BillardsBookingApp/frmMain.cs
+++ BillardsBookingApp/frmMain.cs
@@ -1,10 +1,12 @@
-﻿using BussinessObject.Models;
+﻿using BillardsBookingApp;
+using BussinessObject.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,10 +18,13 @@ namespace SalesWinApp
     {
         public User userOrAdmin { get; set; }
         frmLogin frmLogin;
+        frmApplicationInfo frmApplicationInfo;
         public frmMain()
         {
             InitializeComponent();
             loginToolStripMenuItem.Visible = false;
+            manageToolStripMenuItem.Visible = false;
+            viewToolStripMenuItem.Visible = false;
         }
 
         private void frmMain_Load(object sender, EventArgs e)
@@ -33,19 +38,29 @@ namespace SalesWinApp
             {
                 loginToolStripMenuItem.Visible = true;
                 logoutToolStripMenuItem.Visible = false;
+                menuStripItem_accountInfo.Visible = false;
             }
 
             else if (userOrAdmin != null)
             {
                 loginToolStripMenuItem.Visible = false;
                 logoutToolStripMenuItem.Visible = true;
+                menuStripItem_accountInfo.Visible = true;
+                if (userOrAdmin.Role.Equals("Staff"))
+                {
+                    manageToolStripMenuItem.Visible = true;
+                }
+                else if (userOrAdmin.Role.Equals("Customer"))
+                {
+                    viewToolStripMenuItem.Visible = true;
+                }
             }
         }
 
         private void logoutToolStripMenuItem_Click(object sender, EventArgs e)
         {
             DialogResult d = MessageBox.Show("Do you want to Logout of current account?", "Login Status", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
-            if(d == DialogResult.OK)
+            if (d == DialogResult.OK)
             {
                 userOrAdmin = null;
                 MessageBox.Show("Logout successfully!");
@@ -57,6 +72,20 @@ namespace SalesWinApp
         {
             frmLogin loginForm = new frmLogin(this);
             loginForm.Show();
+        }
+
+        private void applicationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (frmApplicationInfo == null || frmApplicationInfo.IsDisposed)
+            {
+                frmApplicationInfo = new frmApplicationInfo();
+            }
+            frmApplicationInfo.Show();
+        }
+
+        private void menuStripItem_accountInfo_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
